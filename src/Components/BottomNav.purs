3b167002dd5e6e1214a039f84cfd6b788d8215a3
Mike Solomon <mike120982@gmail.com>
2022-11-19 16:27:38 +0200

    Adds back dark mode menu

--- src/Components/BottomNav.purs
+++ src/Components/BottomNav.purs
@@ -1,4 +1,4 @@
-module Components.NextUp where
+module Components.BottomNav where
 
 
 import Data.Foldable (oneOf)
@@ -7,12 +7,30 @@ import Deku.Control (text_)
 import Deku.Core (Domable)
 import Deku.DOM as D
 
-nextUp :: forall lock payload. { link :: String, title :: String } -> Domable lock payload
-nextUp { link, title } = D.dl
+bottomNav :: forall lock payload. { link :: String, title :: String } -> Domable lock payload
+bottomNav { link, title } = D.dl
   ( D.Class !:=
       "mt-12 flex border-t border-slate-200 pt-6 dark:border-slate-800"
   )
-  [ D.div (D.Class !:= "ml-auto text-right")
+  [ D.div_
+  [ D.dt
+      ( D.Class !:=
+          "font-display text-sm font-medium text-slate-900 dark:text-white"
+      )
+      [ text_ "Previous" ]
+  , D.dd (D.Class !:= "mt-1")
+      [ D.a
+          ( oneOf
+              [ D.Class !:=
+                  "text-base font-semibold text-slate-500 hover:text-slate-600 dark:text-slate-400 dark:hover:text-slate-300"
+              , D.Href !:= "/docs/predicting-user-behavior"
+              ]
+          )
+          [ D.span_ [ text_ "←" ]
+          , text_ " Predicting user behavior"
+          ]
+      ]
+  ], D.div (D.Class !:= "ml-auto text-right")
       [ D.dt
           ( D.Class !:=
               "font-display text-sm font-medium text-slate-900 dark:text-white"
--- src/Components/FrontPage.purs
+++ src/Components/FrontPage.purs
@@ -3,10 +3,9 @@ module Components.FrontPage where
 import Prelude
 
 import Assets (blurCyanURL, blurIndigoURL)
+import Components.BottomNav (bottomNav)
 import Components.Disclaimer (disclaimer)
-import Components.NextUp (nextUp)
 import Components.ProTip (proTip)
-import Control.Plus (empty)
 import Data.Foldable (oneOf)
 import Deku.Attribute (xdata, (!:=))
 import Deku.Control (text_)
@@ -190,7 +189,7 @@ everything = fixed
                           [ D.AriaHidden !:= "true"
                           , D.ViewBox !:= "0 0 16 16"
                           , D.Class !:=
-                              "hidden h-4 w-4 fill-sky-400 [[data-theme=light]_&]:block"
+                              "h-4 w-4 fill-sky-400 [[data-theme=light]_&]:block"
                           ]
                       )
                       [ D.path
@@ -256,6 +255,124 @@ everything = fixed
                               ]
                           )
                           []
+                      ]
+                  ]
+              , D.ul
+                  ( oneOf
+                      [ D.Class !:=
+                          "hidden absolute top-full left-1/2 mt-3 w-36 -translate-x-1/2 space-y-1 rounded-xl bg-white p-3 text-sm font-medium shadow-md shadow-black/5 ring-1 ring-black/5 dark:bg-slate-800 dark:ring-white/5"
+                      --, D.AriaLabelledby !:= "headlessui-listbox-label-:R1j36:"
+                      --, D.AriaOrientation !:= "vertical"
+                      , D.Id !:= "headlessui-listbox-options-:R3j36:"
+                      , D.Role !:= "listbox"
+                      , D.Tabindex !:= "0"
+                      , pure (xdata "headlessui-state" "open")
+                      ]
+                  )
+                  [ D.li
+                      ( oneOf
+                          [ D.Class !:=
+                              "flex cursor-pointer select-none items-center rounded-[0.625rem] p-1 text-sky-500"
+                          , D.Id !:= "headlessui-listbox-option-:r6:"
+                          , D.Role !:= "option"
+                          , D.Tabindex !:= "-1"
+                          --, D.AriaSelected !:= "true"
+                          , pure (xdata "headlessui-state" "selected")
+                          ]
+                      )
+                      [ D.div
+                          ( D.Class !:=
+                              "rounded-md bg-white p-1 shadow ring-1 ring-slate-900/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
+                          )
+                          [ D.svg
+                              ( oneOf
+                                  [ D.AriaHidden !:= "true"
+                                  , D.ViewBox !:= "0 0 16 16"
+                                  , D.Class !:= "h-4 w-4 fill-sky-400 dark:fill-sky-400"
+                                  ]
+                              )
+                              [ D.path
+                                  ( oneOf
+                                      [ D.FillRule !:= "evenodd"
+                                      , D.ClipRule !:= "evenodd"
+                                      , D.D !:=
+                                          "M7 1a1 1 0 0 1 2 0v1a1 1 0 1 1-2 0V1Zm4 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm2.657-5.657a1 1 0 0 0-1.414 0l-.707.707a1 1 0 0 0 1.414 1.414l.707-.707a1 1 0 0 0 0-1.414Zm-1.415 11.313-.707-.707a1 1 0 0 1 1.415-1.415l.707.708a1 1 0 0 1-1.415 1.414ZM16 7.999a1 1 0 0 0-1-1h-1a1 1 0 1 0 0 2h1a1 1 0 0 0 1-1ZM7 14a1 1 0 1 1 2 0v1a1 1 0 1 1-2 0v-1Zm-2.536-2.464a1 1 0 0 0-1.414 0l-.707.707a1 1 0 0 0 1.414 1.414l.707-.707a1 1 0 0 0 0-1.414Zm0-8.486A1 1 0 0 1 3.05 4.464l-.707-.707a1 1 0 0 1 1.414-1.414l.707.707ZM3 8a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h1a1 1 0 0 0 1-1Z"
+                                      ]
+                                  )
+                                  []
+                              ]
+                          ]
+                      , D.div (D.Class !:= "ml-3") [ text_ "Light" ]
+                      ]
+                  , D.li
+                      ( oneOf
+                          [ D.Class !:=
+                              "flex cursor-pointer select-none items-center rounded-[0.625rem] p-1 text-slate-700 dark:text-slate-400"
+                          , D.Id !:= "headlessui-listbox-option-:r7:"
+                          , D.Role !:= "option"
+                          , D.Tabindex !:= "-1"
+                          --, D.AriaSelected !:= "false"
+                          , pure (xdata "headlessui-state" "")
+                          ]
+                      )
+                      [ D.div
+                          ( D.Class !:=
+                              "rounded-md bg-white p-1 shadow ring-1 ring-slate-900/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
+                          )
+                          [ D.svg
+                              ( oneOf
+                                  [ D.AriaHidden !:= "true"
+                                  , D.ViewBox !:= "0 0 16 16"
+                                  , D.Class !:= "h-4 w-4 fill-slate-400"
+                                  ]
+                              )
+                              [ D.path
+                                  ( oneOf
+                                      [ D.FillRule !:= "evenodd"
+                                      , D.ClipRule !:= "evenodd"
+                                      , D.D !:=
+                                          "M7.23 3.333C7.757 2.905 7.68 2 7 2a6 6 0 1 0 0 12c.68 0 .758-.905.23-1.332A5.989 5.989 0 0 1 5 8c0-1.885.87-3.568 2.23-4.668ZM12 5a1 1 0 0 1 1 1 1 1 0 0 0 1 1 1 1 0 1 1 0 2 1 1 0 0 0-1 1 1 1 0 1 1-2 0 1 1 0 0 0-1-1 1 1 0 1 1 0-2 1 1 0 0 0 1-1 1 1 0 0 1 1-1Z"
+                                      ]
+                                  )
+                                  []
+                              ]
+                          ]
+                      , D.div (D.Class !:= "ml-3") [ text_ "Dark" ]
+                      ]
+                  , D.li
+                      ( oneOf
+                          [ D.Class !:=
+                              "flex cursor-pointer select-none items-center rounded-[0.625rem] p-1 text-slate-700 dark:text-slate-400"
+                          , D.Id !:= "headlessui-listbox-option-:r8:"
+                          , D.Role !:= "option"
+                          , D.Tabindex !:= "-1"
+                          --, D.AriaSelected !:= "false"
+                          , pure (xdata "headlessui-state" "")
+                          ]
+                      )
+                      [ D.div
+                          ( D.Class !:=
+                              "rounded-md bg-white p-1 shadow ring-1 ring-slate-900/5 dark:bg-slate-700 dark:ring-inset dark:ring-white/5"
+                          )
+                          [ D.svg
+                              ( oneOf
+                                  [ D.AriaHidden !:= "true"
+                                  , D.ViewBox !:= "0 0 16 16"
+                                  , D.Class !:= "h-4 w-4 fill-slate-400"
+                                  ]
+                              )
+                              [ D.path
+                                  ( oneOf
+                                      [ D.FillRule !:= "evenodd"
+                                      , D.ClipRule !:= "evenodd"
+                                      , D.D !:=
+                                          "M1 4a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v4a3 3 0 0 1-3 3h-1.5l.31 1.242c.084.333.36.573.63.808.091.08.182.158.264.24A1 1 0 0 1 11 15H5a1 1 0 0 1-.704-1.71c.082-.082.173-.16.264-.24.27-.235.546-.475.63-.808L5.5 11H4a3 3 0 0 1-3-3V4Zm3-1a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z"
+                                      ]
+                                  )
+                                  []
+                              ]
+                          ]
+                      , D.div (D.Class !:= "ml-3") [ text_ "System" ]
                       ]
                   ]
               ]
@@ -2200,7 +2317,7 @@ export default {
                       ]
                   ]
               ]
-          , nextUp { link: "/docs/installation", title: "Installation" }
+          , bottomNav { link: "/docs/installation", title: "Installation" }
           ]
       , D.div
           ( D.Class !:=
