fa4db92aaa68786989c5ac33d21fc66e61c2990c
Mike Solomon <mike120982@gmail.com>
2022-11-27 07:48:29 +0200

    Comments out algolia stub

--- src/Components/Header.purs
+++ src/Components/Header.purs
@@ -119,42 +119,42 @@ header { setHeaderElement, darkBoolean, dark, setDark, pageIs, pageWas, pushStat
                 []
             ]
         ]
-    , D.div (D.Class !:= "-my-5 mr-6 sm:mr-8 md:mr-0")
-        [ D.button
-            ( oneOf
-                [ D.Xtype !:= "button"
-                , D.Class !:=
-                    "group flex h-6 w-6 items-center justify-center sm:justify-start md:h-auto md:w-80 md:flex-none md:rounded-lg md:py-2.5 md:pl-4 md:pr-3.5 md:text-sm md:ring-1 md:ring-slate-200 md:hover:ring-slate-300 dark:md:bg-slate-800/75 dark:md:ring-inset dark:md:ring-white/5 dark:md:hover:bg-slate-700/40 dark:md:hover:ring-slate-500 lg:w-96"
-                ]
-            )
-            [ D.svg
-                ( oneOf
-                    [ D.AriaHidden !:= "true"
-                    , D.ViewBox !:= "0 0 20 20"
-                    , D.Class !:=
-                        "h-5 w-5 flex-none fill-slate-400 group-hover:fill-slate-500 dark:fill-slate-500 md:group-hover:fill-slate-400"
-                    ]
-                )
-                [ D.path
-                    ( D.D !:=
-                        "M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z"
-                    )
-                    []
-                ]
-            , D.span
-                ( D.Class !:=
-                    "sr-only md:not-sr-only md:ml-2 md:text-slate-500 md:dark:text-slate-400"
-                )
-                [ text_ "Search docs" ]
-            , D.kbd
-                ( D.Class !:=
-                    "ml-auto hidden font-medium text-slate-400 dark:text-slate-500 md:block"
-                )
-                [ D.kbd (D.Class !:= "font-sans") [ text_ "⌘" ]
-                , D.kbd (D.Class !:= "font-sans") [ text_ "K" ]
-                ]
-            ]
-        ]
+    -- , D.div (D.Class !:= "-my-5 mr-6 sm:mr-8 md:mr-0")
+    --     [ D.button
+    --         ( oneOf
+    --             [ D.Xtype !:= "button"
+    --             , D.Class !:=
+    --                 "group flex h-6 w-6 items-center justify-center sm:justify-start md:h-auto md:w-80 md:flex-none md:rounded-lg md:py-2.5 md:pl-4 md:pr-3.5 md:text-sm md:ring-1 md:ring-slate-200 md:hover:ring-slate-300 dark:md:bg-slate-800/75 dark:md:ring-inset dark:md:ring-white/5 dark:md:hover:bg-slate-700/40 dark:md:hover:ring-slate-500 lg:w-96"
+    --             ]
+    --         )
+    --         [ D.svg
+    --             ( oneOf
+    --                 [ D.AriaHidden !:= "true"
+    --                 , D.ViewBox !:= "0 0 20 20"
+    --                 , D.Class !:=
+    --                     "h-5 w-5 flex-none fill-slate-400 group-hover:fill-slate-500 dark:fill-slate-500 md:group-hover:fill-slate-400"
+    --                 ]
+    --             )
+    --             [ D.path
+    --                 ( D.D !:=
+    --                     "M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z"
+    --                 )
+    --                 []
+    --             ]
+    --         , D.span
+    --             ( D.Class !:=
+    --                 "sr-only md:not-sr-only md:ml-2 md:text-slate-500 md:dark:text-slate-400"
+    --             )
+    --             [ text_ "Search docs" ]
+    --         , D.kbd
+    --             ( D.Class !:=
+    --                 "ml-auto hidden font-medium text-slate-400 dark:text-slate-500 md:block"
+    --             )
+    --             [ D.kbd (D.Class !:= "font-sans") [ text_ "⌘" ]
+    --             , D.kbd (D.Class !:= "font-sans") [ text_ "K" ]
+    --             ]
+    --         ]
+    --     ]
     , D.div
         ( D.Class !:=
             "relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow"
