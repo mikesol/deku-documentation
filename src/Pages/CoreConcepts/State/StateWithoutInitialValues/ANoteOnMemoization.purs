56dd639a0e2a4f49eac6608e52145c7b310a8028
Mike Solomon <mike120982@gmail.com>
2022-11-29 00:01:30 +0200

    Adds section

--- src/Pages/CoreConcepts/State/StateWithoutInitialValues/ANoteOnMemoization.purs
+++ src/Pages/CoreConcepts/State/StateWithoutInitialValues/ANoteOnMemoization.purs
@@ -1,9 +1,28 @@
 module Pages.CoreConcepts.State.StateWithoutInitialValues.ANoteOnMemoization where
 
+import Prelude
+
 import Contracts (Env(..), Subsection, subsection)
-import Deku.Control (text_)
+import Data.String (replaceAll, Pattern(..), Replacement(..))
+import Data.Tuple.Nested ((/\))
+import Deku.Attributes (klass_)
+import Deku.Control (guard, text, text_)
 import Deku.DOM as D
+import Deku.Do as Deku
+import Deku.Hooks (useHot, useState)
+import Deku.Listeners (click, click_)
+import Effect.Random (random)
+import QualifiedDo.Alt as Alt
 import Router.ADT (Route(..))
+
+buttonClass :: String -> String
+buttonClass color =
+  replaceAll (Pattern "COLOR") (Replacement color)
+    """mr-4 inline-flex items-center rounded-md
+border border-transparent bg-COLOR-600 px-3 py-2
+text-sm font-medium leading-4 text-white shadow-sm
+hover:bg-COLOR-700 focus:outline-none focus:ring-2
+focus:ring-COLOR-500 focus:ring-offset-2"""
 
 aNoteOnMemoization :: forall lock payload. Subsection lock payload
 aNoteOnMemoization = subsection
@@ -17,7 +36,57 @@ aNoteOnMemoization = subsection
           , D.b__ "A"
           , text_ " a few times before pressing button "
           , D.b__ "B"
-          , text_ ". What do you think will happen?"
+          , text_ ", and then after pressing "
+          , D.b__ "B"
+          , text_ " once, press"
+          , D.b__ "A"
+          , text_ " again. What do you think will happen?"
+          ]
+      , D.blockquote (klass_ "not-italic")
+          [ Deku.do
+              setNumber /\ number <- useState 0.42
+              setPresence /\ presence <- useState false
+              D.div_
+                [ D.div_
+                    [ text $ number <#> show >>>
+                        ("Here's a random number: " <> _)
+                    ]
+                , D.div_
+                    [ D.button
+                        Alt.do
+                          klass_ $ buttonClass "pink"
+                          click_ $ random >>= setNumber
+                        [ text_ "A"
+                        ]
+                    , D.button
+                        Alt.do
+                          klass_ $ buttonClass "green"
+                          click $ presence <#> not >>> setPresence
+                        [ text_ "B"
+                        ]
+                    ]
+                , D.div_
+                    [ guard presence
+                        $ text
+                        $ number <#> show >>>
+                            ("Here's the same (?) random number: " <> _)
+                    ]
+                ]
+          ]
+      , D.p_
+          [ text_
+              "Because the hook simply passes through the values as it receives them, when "
+          , D.b__ "B"
+          , text_
+              " was pressed, and as there was no simultaneous value coming from "
+          , D.b__ "A"
+          , text_ ", the guarded section didn't activate until "
+          , D.b__ "A"
+          , text_
+              " was pressed again. In effect, while the hook had an initial value for the first "
+          , D.code__ "text"
+          , text_
+              "bloc, it lacked an initial value for any bloc that was created afterwards. You can think of initial values to hooks as being relevant only at the moment of creation. Every subsequent usage of them is \"valueless\" until initialized."
           ]
       , D.p_
           [ text_
@@ -29,6 +98,37 @@ aNoteOnMemoization = subsection
           , text_ ", you can click "
           , D.b__ "Reveal code"
           , text_ "."
+          ]
+      , D.blockquote (klass_ "not-italic")
+          [ Deku.do
+              setNumber /\ number <- useHot 0.42
+              setPresence /\ presence <- useState false
+              D.div_
+                [ D.div_
+                    [ text $ number <#> show >>>
+                        ("Here's a random number: " <> _)
+                    ]
+                , D.div_
+                    [ D.button
+                        Alt.do
+                          klass_ $ buttonClass "pink"
+                          click_ $ random >>= setNumber
+                        [ text_ "A"
+                        ]
+                    , D.button
+                        Alt.do
+                          klass_ $ buttonClass "green"
+                          click $ presence <#> not >>> setPresence
+                        [ text_ "B"
+                        ]
+                    ]
+                , D.div_
+                    [ guard presence
+                        $ text
+                        $ number <#> show >>>
+                            ("Here's the same (?) random number: " <> _)
+                    ]
+                ]
           ]
       ]
   }
--- src/Scratch.purs
+++ src/Scratch.purs
@@ -2,24 +2,51 @@ module Scratch where
 
 import Prelude
 
+import Data.String (replaceAll, Pattern(..), Replacement(..))
 import Data.Tuple.Nested ((/\))
+import Deku.Attributes (klass_)
 import Deku.Control (guard, text, text_)
 import Deku.DOM as D
 import Deku.Do as Deku
 import Deku.Hooks (useState)
-import Deku.Listeners (click)
+import Deku.Listeners (click, click_)
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
+import Effect.Random (random)
+import QualifiedDo.Alt as Alt
+
+buttonClass :: String -> String
+buttonClass color =
+  replaceAll (Pattern "COLOR") (Replacement color)
+    """ml-4 inline-flex items-center rounded-md
+border border-transparent bg-COLOR-600 px-3 py-2
+text-sm font-medium leading-4 text-white shadow-sm
+hover:bg-COLOR-700 focus:outline-none focus:ring-2
+focus:ring-COLOR-500 focus:ring-offset-2"""
 
 main :: Effect Unit
 main = runInBody Deku.do
+  setNumber /\ number <- useState 0.42
   setPresence /\ presence <- useState true
   D.div_
-    [ guard presence (text_ "Now you see me, ")
-    , D.a
-        Alt.do
-          click $ presence <#> not >>> setPresence
-        [ text $ presence <#>
-            if _ then "now you don't." else "Oops, come back!"
+    [ D.div_
+        [ text $ number <#> show >>> ("Here's a random number: " <> _)
+        , D.button
+            Alt.do
+              klass_ $ buttonClass "pink"
+              click_ $ random >>= setNumber
+            [ text_ "A"
+            ]
+        , D.button
+            Alt.do
+              klass_ $ buttonClass "green"
+              click $ presence <#> not >>> setPresence
+            [ text_ "B"
+            ]
+        ]
+    , D.div_
+        [ guard presence
+            $ text
+            $ number <#> show >>> ("Here's the same (?) random number: " <> _)
         ]
     ]
