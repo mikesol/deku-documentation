c005a45a8b921d140988e7102baf2577412a9bbf
Mike Solomon <mike120982@gmail.com>
2022-12-01 15:45:22 +0200

    Adds global vid

--- src/Pages/CoreConcepts/Portals/LocalPortals/PerformanceNotes.purs
+++ src/Pages/CoreConcepts/Portals/LocalPortals/PerformanceNotes.purs
@@ -12,9 +12,28 @@ performanceNotes = subsection
   { title: "Performance notes"
   , matter: pure
       [ D.p_
-          [ text_ "This subsection will be about "
-          , D.span (D.Class !:= "font-bold") [ text_ "Performance notes" ]
-          , text_ "."
+          [ text_
+              "Had we used "
+          , D.code__ "globalPortal"
+          , text_
+              " for the inner portal, every time the video was shuffled, a new portal would have been created "
+          , D.i__ "without"
+          , text_ " the old one being cleaned up. That means that, if we clicked on "
+          , D.b__ "Shuffle video"
+          , text_
+              " 100 times, 100 \"zombie\" videos would be hanging out in JavaScript, eating up memory and CPU."
           ]
+      , D.p_
+          [ text_
+              "In a HTML5 game like a first-person shooter, if you use videos for VFX, you're likely creating 10s of VFX a second. In most cases, these won't be portals, in which case Deku's memory management system will handle them automatically. However, if they "
+          , D.i__ "are"
+          , text_
+              " portals, you want them to be cleaned up as soon as their parents go off screen. In these cases, make sure to use "
+          , D.code__ "portal1"
+          , text_ " instead of "
+          , D.code__ "globalPortal1"
+          , text_ "!"
+          ]
+
       ]
   }
--- src/Pages/CoreConcepts/Portals/LocalPortals/UnlockingLevels.purs
+++ src/Pages/CoreConcepts/Portals/LocalPortals/UnlockingLevels.purs
@@ -3,13 +3,14 @@ module Pages.CoreConcepts.Portals.LocalPortals.UnlockingLevels where
 import Prelude
 
 import Components.Code (psCode)
+import Constants (tripleQ)
 import Contracts (Subsection, subsection)
 import Data.Foldable (oneOf)
 import Data.String (replaceAll, Pattern(..), Replacement(..))
 import Data.Tuple.Nested ((/\))
-import Deku.Attribute ((!:=))
+import Deku.Attribute ((!:=), (:=))
 import Deku.Attributes (klass_)
-import Deku.Control (guard, portal1, text, text_, (<#~>))
+import Deku.Control (globalPortal1, guard, portal1, text, text_, (<#~>))
 import Deku.Core (Domable)
 import Deku.DOM as D
 import Deku.Do as Deku
@@ -18,6 +19,8 @@ import Deku.Listeners (click, click_)
 import Effect (Effect)
 import FRP.Event (Event)
 import QualifiedDo.Alt as Alt
+import Web.HTML.HTMLMediaElement (play)
+import Web.HTML.HTMLVideoElement (toHTMLMediaElement)
 
 data Square = TL | BL | TR | BR
 
@@ -53,14 +56,19 @@ vid2URL Video2 = "https://media.giphy.com/media/3o6UB7jHQYIjeh5IGY/giphy.mp4"
 vid2URL Video3 = "https://media.giphy.com/media/12GbJUnssN6NTa/giphy.mp4"
 vid2URL Video4 = "https://media.giphy.com/media/7T8LajxmBkz8gmYcRB/giphy.mp4"
 
-myVideo :: forall lock payload. String -> Domable lock payload
-myVideo vid = D.video
+myVideo :: forall lock payload. Event Boolean -> String -> Domable lock payload
+myVideo bang vid = D.video
   ( oneOf
       [ D.Width !:= "175"
       , D.Height !:= "175"
       , D.Autoplay !:= "true"
       , D.Loop !:= "true"
       , D.Muted !:= "true"
+      , bang <#> \tf -> D.SelfT := \e ->
+          if not tf then pure unit
+          else do
+            let melt = toHTMLMediaElement e
+            play melt
       ]
   )
   [ D.source (D.Src !:= vid)
@@ -85,18 +93,22 @@ example =
 import Prelude
 
 import Data.Foldable (oneOf)
+import Data.String (replaceAll, Pattern(..), Replacement(..))
 import Data.Tuple.Nested ((/\))
-import Deku.Attribute ((!:=))
+import Deku.Attribute ((!:=), (:=))
 import Deku.Attributes (klass_)
-import Deku.Control (portal1, guard, text_)
-import Deku.Core (Domable, Nut)
+import Deku.Control (globalPortal1, guard, portal1, text, text_, (<#~>))
+import Deku.Core (Domable)
 import Deku.DOM as D
 import Deku.Do as Deku
-import Deku.Hooks (useState)
-import Deku.Listeners (click_)
+import Deku.Hooks (useHot, useState)
+import Deku.Listeners (click, click_)
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
 import FRP.Event (Event)
+import QualifiedDo.Alt as Alt
+import Web.HTML.HTMLMediaElement (play)
+import Web.HTML.HTMLVideoElement (toHTMLMediaElement)
 
 data Square = TL | BL | TR | BR
 
@@ -126,29 +138,97 @@ moveSpriteHere { video, square, setSquare, at } = D.a
       ]
   ]
 
-myVideo :: Nut
-myVideo = D.video
+vid2URL :: WhichVideo -> String
+vid2URL Video1 = "https://media.giphy.com/media/IMSq59ySKydYQ/giphy.mp4"
+vid2URL Video2 = "https://media.giphy.com/media/3o6UB7jHQYIjeh5IGY/giphy.mp4"
+vid2URL Video3 = "https://media.giphy.com/media/12GbJUnssN6NTa/giphy.mp4"
+vid2URL Video4 = "https://media.giphy.com/media/7T8LajxmBkz8gmYcRB/giphy.mp4"
+
+myVideo :: forall lock payload. Event Boolean -> String -> Domable lock payload
+myVideo bang vid = D.video
   ( oneOf
       [ D.Width !:= "175"
       , D.Height !:= "175"
       , D.Autoplay !:= "true"
       , D.Loop !:= "true"
       , D.Muted !:= "true"
+      , bang <#> \tf -> D.SelfT := \e ->
+          if not tf then pure unit
+          else do
+            let melt = toHTMLMediaElement e
+            play melt
       ]
   )
-  [ D.source (D.Src !:= "https://media.giphy.com/media/IMSq59ySKydYQ/giphy.mp4")
+  [ D.source (D.Src !:= vid)
       []
   ]
 
+buttonClass :: String -> String
+buttonClass color =
+  replaceAll (Pattern "COLOR") (Replacement color)
+    """ <> tripleQ
+    <>
+      """mr-4 inline-flex items-center rounded-md
+border border-transparent bg-COLOR-600 px-3 py-2
+text-sm font-medium leading-4 text-white shadow-sm
+hover:bg-COLOR-700 focus:outline-none focus:ring-2
+focus:ring-COLOR-500 focus:ring-offset-2"""
+    <> tripleQ
+    <>
+      """
+
+data WhichVideo = Video1 | Video2 | Video3 | Video4
+
 main :: Effect Unit
 main = runInBody Deku.do
-  portal1 myVideo \(vid /\ _) -> Deku.do
-    setSquare /\ square <- useState TL
-    D.div (klass_ "grid grid-cols-2")
-      [ moveSpriteHere { video: vid, square, setSquare, at: TL }
-      , moveSpriteHere { video: vid, square, setSquare, at: TR }
-      , moveSpriteHere { video: vid, square, setSquare, at: BL }
-      , moveSpriteHere { video: vid, square, setSquare, at: BR }
+  setVideoURL /\ videoURL <- useHot Video1
+  setGlobalVideoPresence /\ globalVideoPresence <- useHot true
+  let
+    rotator Video1 = Video2
+    rotator Video2 = Video3
+    rotator Video3 = Video4
+    rotator Video4 = Video1
+  globalPortal1
+    ( myVideo (pure true)
+        "https://media.giphy.com/media/3o6Zt6GFP75DlxnDXy/giphy.mp4"
+    )
+    \globalVid -> D.div_
+      [ D.div (klass_ "flex")
+          [ guard globalVideoPresence $ D.button
+              Alt.do
+                klass_ $ buttonClass "indigo"
+                click $ videoURL <#> rotator >>> setVideoURL
+              [ text_ "Shuffle video" ]
+          , D.button
+              Alt.do
+                klass_ $ buttonClass "indigo"
+                click $ globalVideoPresence <#> not >>>
+                  setGlobalVideoPresence
+              [ text $ globalVideoPresence <#>
+                  if _ then "Send this video down ->"
+                  else "Bring video back up"
+              ]
+          , guard globalVideoPresence globalVid
+          ]
+      , D.div_
+          [ videoURL <#~> \v -> portal1
+              (myVideo globalVideoPresence (vid2URL v))
+              \(vid /\ lowerVid) -> Deku.do
+                setSquare /\ square <- useState TL
+                let
+                  switchable = globalVideoPresence <#~>
+                    if _ then vid else (lowerVid globalVid)
+                D.div (klass_ "grid grid-cols-2")
+                  [ moveSpriteHere
+                      { video: switchable, square, setSquare, at: TL }
+                  , moveSpriteHere
+                      { video: switchable, square, setSquare, at: TR }
+                  , moveSpriteHere
+                      { video: switchable, square, setSquare, at: BL }
+                  , moveSpriteHere
+                      { video: switchable, square, setSquare, at: BR }
+                  ]
+          ]
       ]"""
 
 unlockingLevels :: forall lock payload. Subsection lock payload
@@ -157,35 +237,44 @@ unlockingLevels = subsection
   , matter: pure
       [ D.p_
           [ text_
-              "Local portals look a lot like global portals with two important caveats."
-          , D.ol_
-              [ D.li_
-                  [ text_
-                      "They must be written as functions instead of using the "
-                  , D.code__ "do"
-                  , text_ " syntax with a left bind."
-                  ]
-              , D.li_
-                  [ text_
-                      "They output a additional parameter in addition to the component. We'll see what that parameter is a bit later, but for now we'll ignore it."
-                  ]
-              ]
+              "Local portals have one big advantage over global portals: they get cleaned up when the last reference to them is deleted. Because global portals can be used "
+          , D.i__ "anywhere"
+          , text_
+              " in your application at any time, it's impossible to know when they can be safely deleted. Local portals, on the other hand, are tied to the scope in which they are created and cannot be used at a higher scope. If you try, for example by sending them to a hook, you'll get a type error."
+          ]
+
+      , D.p_
+          [ text_
+              "Portals from a higher scope, on the other hand, can be used in a lower scope because, by definition, for the lower scope to exist, the higher scope must still exist. However, to use portals at lower scopes, you need to pass them through a type-checking function that validates they can be used. This function is the right side of the tuple in the closure passed to"
+          , D.code__ "portal1"
+          , text_
+              ". This function is optimized out of builds at compile time by the PureScript backend optimizer, so there is no runtime penalty when called."
+          ]
+      , D.p_
+          [ text_
+              "In the code below, note how we use this mechanism to call "
+          , D.code__ "lowerVid"
+          , text_
+              " when we want to use the video "
+          , D.code__ "globalVid"
+          , text_ " from the higher scope inside of the lower scope."
           ]
       , psCode example
-      , D.p__ "This yields a similar result as the one above."
+      , D.p__
+          "In the example below, make sure to try all the controls. Send the top video down, bring the video back up again, shuffle the inner videos, etc. While you can't see it, know that every time you click shuffle video, the inner video object's memory is released when it is replaced."
       , Deku.do
-          setVideoURL /\ videoURL <- useState Video1
+          setVideoURL /\ videoURL <- useHot Video1
           setGlobalVideoPresence /\ globalVideoPresence <- useHot true
           let
             rotator Video1 = Video2
             rotator Video2 = Video3
             rotator Video3 = Video4
             rotator Video4 = Video1
-          portal1
-            ( myVideo
+          globalPortal1
+            ( myVideo (pure true)
                 "https://media.giphy.com/media/3o6Zt6GFP75DlxnDXy/giphy.mp4"
             )
-            \(globalVid /\ _) -> D.div_
+            \globalVid -> D.div_
               [ D.div (klass_ "flex")
                   [ guard globalVideoPresence $ D.button
                       Alt.do
@@ -204,7 +293,8 @@ unlockingLevels = subsection
                   , guard globalVideoPresence globalVid
                   ]
               , D.div_
-                  [ videoURL <#~> \v -> portal1 (myVideo (vid2URL v))
+                  [ videoURL <#~> \v -> portal1
+                      (myVideo globalVideoPresence (vid2URL v))
                       \(vid /\ lowerVid) -> Deku.do
                         setSquare /\ square <- useState TL
                         let
@@ -222,9 +312,5 @@ unlockingLevels = subsection
                           ]
                   ]
               ]
-      , D.p_
-          [ text_
-              "Aside from these differences, local portals look a lot like global ones. However, for a bit extra syntax, you get better performance. Let's see how in the next section."
-          ]
       ]
   }
