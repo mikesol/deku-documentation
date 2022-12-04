e1d92efe9e19f660613dc6fa7cb3d4dd52ac4851
Mike Solomon <mike120982@gmail.com>
2022-12-04 18:16:15 +0200

    Adds extra note

--- src/Pages/FRP/Events/OptimizedEvents/OptimizedEventFunctions.purs
+++ src/Pages/FRP/Events/OptimizedEvents/OptimizedEventFunctions.purs
@@ -176,7 +176,7 @@ optimizedEventFunctions = subsection
           [ text_
               "You'll see that the optimized version has less thunks - there is one less "
           , D.code__ "do"
-          , text_ " bloc, and the callback does not need a thunk anymore."
+          , text_ " bloc, the subscribe function is optimized out completely, and the callback does not need a thunk anymore."
           ]
       , D.h4__ "Unoptimized"
       , jsCode
