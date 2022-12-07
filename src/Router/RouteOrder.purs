b1fab4d1224128711cb2d17d314732a71a7f842e
Mike Solomon <mike120982@gmail.com>
2022-12-07 18:42:15 +0200

    Clarifies naming

---
+++ src/Router/RouteOrder.purs
@@ -0,0 +1,30 @@
+module Router.RouteOrder where
+
+import Prelude
+
+import Data.Array (drop, zip)
+import Data.Map (Map, fromFoldable, lookup)
+import Data.Maybe (Maybe(..))
+import Data.Newtype (unwrap)
+import Pages.Docs (docs)
+import Router.ADT (Route(..))
+
+pageOrder :: Array Route
+pageOrder = join $ map (unwrap >>> _.pages >>> map (unwrap >>> _.route))
+  (unwrap docs)
+
+pageOrderMinusOne = drop 1 pageOrder
+
+routeToNextRouteMap :: Map Route Route 
+routeToNextRouteMap = fromFoldable (zip pageOrder pageOrderMinusOne)
+
+routeToPrevRouteMap :: Map Route Route 
+routeToPrevRouteMap = fromFoldable (zip pageOrderMinusOne pageOrder)
+
+routeToNextRoute :: Route -> Maybe Route 
+routeToNextRoute FourOhFour = Nothing
+routeToNextRoute a =  lookup a routeToNextRouteMap
+
+routeToPrevRoute :: Route -> Maybe Route
+routeToPrevRoute FourOhFour = Nothing
+routeToPrevRoute a = lookup a routeToPrevRouteMap
