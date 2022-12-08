352f89a5323e9b7cabcbeed833eb92766b813f22
Mike Solomon <mike120982@gmail.com>
2022-12-08 07:35:41 +0200

    Adds src

--- src/Components/Code.purs
+++ src/Components/Code.purs
@@ -4,12 +4,13 @@ import Prelude
 
 import Control.Plus (empty)
 import Deku.Attribute (Attribute, (!:=))
-import Deku.Attributes (klass_)
+import Deku.Attributes (href_, klass_)
 import Deku.Control (text_)
 import Deku.Core (Domable)
 import Deku.DOM as D
-import Examples (ExampleADT, exampleToString)
+import Examples (ExampleADT, exampleToSlug, exampleToString)
 import FRP.Event (Event)
+import QualifiedDo.Alt as Alt
 
 jsCode :: forall lock payload. String -> Domable lock payload
 jsCode code = D.pre (D.Class !:= "prism-code language-javascript")
@@ -59,12 +60,49 @@ psCodeWithLink'
   -> Event (Attribute D.Code_)
   -> ExampleADT
   -> Domable lock17 payload18
-psCodeWithLink' s e ex = D.pre
-  (D.Class !:= ("prism-code language-purescript" <> s))
-  [ D.code e
-      [ text_ (exampleToString ex)
+psCodeWithLink' s e ex = D.div_
+  [ D.div_
+      [ D.a
+          Alt.do
+            klass_ "text-sm"
+            href_
+              ( "https://gitpod.io/#START=" <> slug <>
+                  "/https://github.com/my-org/repo-to-work-on"
+              )
+            D.Target !:= "_blank"
+          [ text_ "Run on Gitpod" ]
+      , D.span
+          Alt.do
+            klass_ "text-sm"
+          [ text_ " | " ]
+      , D.a
+          Alt.do
+            klass_ "text-sm"
+            href_
+              ( "https://github.com/mikesol/deku-documentation/blob/main/src/Examples/"
+                  <> slug
+                  <> ".purs"
+              )
+            D.Target !:= "_blank"
+          [ text_ "View on Github" ]
+      , D.span
+          Alt.do
+            klass_ "text-sm"
+          [ text_ " | " ]
+      , D.code
+          Alt.do
+            klass_ "text-sm"
+          [ text_ $ "START=" <> slug <> " && npm run dev" ]
+      ]
+  , D.pre
+      (D.Class !:= ("prism-code language-purescript" <> s))
+      [ D.code e
+          [ text_ (exampleToString ex)
+          ]
       ]
   ]
+  where
+  slug = exampleToSlug ex
 
 psCodeWithLink :: forall lock payload. ExampleADT -> Domable lock payload
 psCodeWithLink = psCodeWithLink' "" empty
