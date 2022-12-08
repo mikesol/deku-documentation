119d3fef056781805e1515bc0d930cc7ce94b951
Mike Solomon <mike@meeshkan.com>
2022-12-08 17:03:00 +0200

    Update Definition.purs

--- src/Pages/FRP/Behaviors/Definition.purs
+++ src/Pages/FRP/Behaviors/Definition.purs
@@ -16,7 +16,7 @@ definition = section
   , topmatter: pure
       [ D.p_
           [ text_
-              "This section will look at how behaviors are defined, explore the nuance of the defintiion, and present a small example of a Behavior being used in the browser."
+              "This section will look at how behaviors are defined, explore the nuance of the definition, and present a small example of a Behavior being used in the browser."
           ]
       ]
   , subsections:
