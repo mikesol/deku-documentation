5afc5ce80a00b241668bda409ad14500ee5c8435
Mike Solomon <mike@meeshkan.com>
2022-12-09 08:15:35 +0200

    Update AMailboxingApp.purs

--- src/Pages/CoreConcepts/MoreHooks/UseMailboxed/AMailboxingApp.purs
+++ src/Pages/CoreConcepts/MoreHooks/UseMailboxed/AMailboxingApp.purs
@@ -16,16 +16,16 @@ aMailboxingApp = subsection
           [ text_ "While it's possible to achieve a similar outcome with "
           , routeLink Filtering
           , text_
-              ", it is much slower. Individual filters execute in "
+              ", it's much slower. Individual filters for a collection of ", D.i__ "n", text_ " items execute in "
           , D.i__ "O(n)"
           , text_
-              " because each entry has to check if it has a valid input."
+              " time because each entry has to check if it has a valid input."
           ]
       , D.p_
           [ text_
-              "With mailbox, the postwoman is pre-sorting the mail and only delivring it to valid inputs. The sorting function is "
+              "With mailbox, the postwoman is pre-sorting the mail and only delivring it to valid destinations. The sorting function executes in "
           , D.i__ "O(logn)"
-          , text_ " thanks to the "
+          , text_ " time thanks to the "
           , D.code__ "Ord"
           , text_
               " constraint on the input. So when woring with collections where you'll have to update a single addressable element in response to an event, use mailboxes!"
