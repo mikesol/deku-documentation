f98135d2e1d8edcc10e69717f4d4e2d5280268fc
Mike Solomon <mike120982@gmail.com>
2022-12-09 13:27:37 +0200

    Small doc change

--- src/Pages/Introduction/GettingStarted/QuickStart/UsingAnExistingProject.purs
+++ src/Pages/Introduction/GettingStarted/QuickStart/UsingAnExistingProject.purs
@@ -3,6 +3,7 @@ module Pages.Introduction.GettingStarted.QuickStart.UsingAnExistingProject where
 import Prelude
 
 import Components.Code (bashCode)
+import Components.Disclaimer (disclaimer)
 import Contracts (Subsection, subsection)
 import Deku.Control (text_)
 import Deku.DOM as D
@@ -21,5 +22,20 @@ usingAnExistingProject = subsection
               "Once PureScript is installed, or if you're working from a project with PureScript already installed, you can install Deku with the following command."
           ]
       , bashCode "npx spago install deku"
+      , disclaimer
+          { header: text_ "Package sets"
+          , message:
+              D.div_
+                [ text_
+                    "PureScript uses package sets to make sure that a codebase has a coherent set of packages. This documentation works for Deku version "
+                , D.code__ "0.9.8"
+                , text_
+                    " onwards, so if you'd like to use most of the conventions from this documentation, make sure to work with a package set that's made on or after December 9, 2022 (AD). You can update your package set in the "
+                , D.code__ "packages.dhall"
+                , text_ " file that is created when you run."
+                , D.code__ "spago init"
+                , text_ "."
+                ]
+          }
       ]
   }
