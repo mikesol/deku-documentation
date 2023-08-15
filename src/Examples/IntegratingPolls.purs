725de647903caf0635277383cf1e70f2eda2de30
Mike Solomon <mike@meeshkan.com>
2023-08-15 07:15:05 +0300

    Progress

--- src/Examples/IntegratingPolls.purs
+++ src/Examples/IntegratingPolls.purs
@@ -1,23 +1,22 @@
 module Examples.IntegratingPolls where
 
-import Deku.Toplevel (runInBody')
-import Effect (Effect)
 import Prelude
-import ExampleAssitant (ExampleSignature)
 
 import Data.Time.Duration (Seconds(..))
 import Data.Tuple.Nested ((/\))
-import Deku.Attribute ((:=), (<:=>), (!:=))
-import Deku.Attributes (klass, klass_)
-import Deku.Control (text, text_)
+import Deku.Attribute ((!:=))
+import Deku.Attributes (klass_)
+import Deku.Control (text)
 import Deku.DOM as D
 import Deku.Do as Deku
 import Deku.Hooks (useState)
-import Deku.Listeners (slider)
-
-import FRP.Poll (integral', sample_, stepNE)
-import FRP.Poll.Time (seconds)
+import Deku.Listeners (slider_)
+import Deku.Toplevel (runInBody')
+import Effect (Effect)
+import ExampleAssitant (ExampleSignature)
 import FRP.Event.AnimationFrame (animationFrame)
+import FRP.Poll (integral', sample_, sham)
+import FRP.Poll.Time (seconds)
 
 app :: ExampleSignature
 app runExample = do
@@ -27,27 +26,28 @@ app runExample = do
     D.div_
       [ D.div_
           [ D.input
-              ( slider setNumber <>
-                  [ klass_ "w-full"
-                  , D.Min !:= "0.0"
-                  , D.Max !:= "1.0"
-                  , D.Step !:= "0.01"
-                  , D.Value !:= "0.0"
-                  ]
-              )
+              [ slider_ setNumber
+              , klass_ "w-full"
+              , D.Min !:= "0.0"
+              , D.Max !:= "1.0"
+              , D.Step !:= "0.01"
+              , D.Value !:= "0.0"
+              ]
               []
           ]
       , D.div_
           [ text
-              ( show <$>
-                  ( ( sample_
-                        ( integral' 0.0
-                            (seconds <#> (\(Seconds s) -> s))
-                            (stepNE number)
+              ( sham
+                  ( show <$>
+                      ( ( sample_
+                            ( integral' 0.0
+                                (seconds <#> (\(Seconds s) -> s))
+                                number
+                            )
+                            af.event
                         )
-                        af.event
-                    )
 
+                      )
                   )
               )
           ]
--- src/Examples/InterComponentCommunication.purs
+++ src/Examples/InterComponentCommunication.purs
@@ -1,24 +1,23 @@
 module Examples.InterComponentCommunication where
 
-import Deku.Toplevel (runInBody')
-import Effect (Effect)
 import Prelude
-import ExampleAssitant (ExampleSignature)
 
 import Data.Foldable (for_, traverse_)
 import Data.Int (floor)
 import Data.String (Pattern(..), Replacement(..), replaceAll)
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
-import Deku.Attribute (cb, (:=))
-import Deku.Attributes (klass, klass_)
-import Deku.Control (text, text_)
+import Deku.Attribute (cb, (!:=))
+import Deku.Attributes (klass_)
+import Deku.Control (text_)
 import Deku.DOM as D
 import Deku.Do as Deku
-import Deku.Hooks (dynOptions, useDynWith_, useState, useState')
-import Deku.Listeners (click, keyUp)
-
-import FRP.Poll (sampleBy, stepNE)
+import Deku.Hooks (dynOptions, useDynWith, useState, useState')
+import Deku.Listeners (click, click_, keyUp)
+import Deku.Toplevel (runInBody')
+import Effect (Effect)
+import ExampleAssitant (ExampleSignature)
+import FRP.Event.Class ((<|*>))
 import Web.Event.Event (target)
 import Web.HTML (window)
 import Web.HTML.HTMLInputElement (fromEventTarget, value, valueAsNumber)
@@ -59,12 +58,12 @@ app runExample = runExample Deku.do
       D.div_
         [ D.input
             [ D.Value !:= "Tasko primo"
-            , keyUp \evt -> do
+            , keyUp $ pure \evt -> do
                 when (code evt == "Enter") $
                   for_
                     ((target >=> fromEventTarget) (toEvent evt))
                     guardAgainstEmpty
-            , D.SelfT := setInput
+            , D.SelfT !:= setInput
             , klass_ inputKls
             ]
             []
@@ -73,7 +72,7 @@ app runExample = runExample Deku.do
             , D.Xtype !:= "number"
             , D.Min !:= "0"
             , D.Value !:= "0"
-            , D.OnChange := cb \evt ->
+            , D.OnChange !:= cb \evt ->
                 traverse_ (valueAsNumber >=> floor >>> setPos) $
                   (target >=> fromEventTarget) evt
             ]
@@ -88,24 +87,23 @@ app runExample = runExample Deku.do
     [ top
     , Deku.do
         { value: t, sendTo, remove } <-
-          useDynWith_
-            (sampleBy Tuple (stepNE pos) item)
-            $ dynOptions { remove = const removeAll }
+          useDynWith
+            (Tuple <$> pos <|*> item) $ dynOptions { remove = const removeAll }
         D.div_
-          [ text t
+          [ text_ t
           , D.button
-              [ klass $ "ml-2 " <> buttonClass "indigo"
-              , click (sendTo 0)
+              [ klass_ $ "ml-2 " <> buttonClass "indigo"
+              , click_ (sendTo 0)
               ]
               [ text_ "Prioritize" ]
           , D.button
-              [ klass $ "ml-2 " <> buttonClass "pink"
-              , click remove
+              [ klass_ $ "ml-2 " <> buttonClass "pink"
+              , click_ remove
               ]
               [ text_ "Delete" ]
           , D.button
-              [ klass $ "ml-2 " <> buttonClass "fuchsia"
-              , click (setRemoveAll unit)
+              [ klass_ $ "ml-2 " <> buttonClass "fuchsia"
+              , click_ (setRemoveAll unit)
               ]
               [ text_ "Remove all" ]
           ]
