63ffed31956931d58c84d4d436b799d33cd96cac
Charles Taylor <charlestaylor95@gmail.com>
2023-08-24 07:31:34 -0400

    Fix typos (#17)
    * Fix typo: `woring -> working`
    
    * Space for `O(log n)`

--- src/Pages/CoreConcepts/MoreHooks/UseMailboxed/AMailboxingApp.purs
+++ src/Pages/CoreConcepts/MoreHooks/UseMailboxed/AMailboxingApp.purs
@@ -25,11 +25,11 @@ aMailboxingApp = subsection
       , D.p_
           [ text_
               "With mailbox, the postwoman is pre-sorting the mail and only delivring it to valid destinations. The sorting function executes in "
-          , D.i__ "O(logn)"
+          , D.i__ "O(log n)"
           , text_ " time thanks to the "
           , D.code__ "Ord"
           , text_
-              " constraint on the input. So when woring with collections where you'll have to update a single addressable element in response to an event, use mailboxes!"
+              " constraint on the input. So when working with collections where you'll have to update a single addressable element in response to an event, use mailboxes!"
           ]
       ]
   }
