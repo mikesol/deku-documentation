063180301837316c08cd1bf65a1ba6046118c365
Mike Solomon <mike.solomon@hey.com>
2024-08-05 08:52:18 +0300

    Adds prerender

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import deku from './rollup-plugin-vike-deku';
 import vercel from 'vite-plugin-vercel';
 
 export default {
-  plugins: [deku(), vike(), vercel()],
+  plugins: [deku(), vike({ prerender: true }), vercel()],
   resolve: {
     alias: {
       PureScript: path.resolve(__dirname, "output"),
