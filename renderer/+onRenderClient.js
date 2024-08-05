e5aeb578fc50ffae0f24639531076e4d19fc3c36
Mike Solomon <mike.solomon@hey.com>
2024-08-05 22:22:25 +0300

    Revert "Moves element definition in window"
    This reverts commit fb9e062662e40fc43bc6ea4be9c06c00b01d7e11.

--- renderer/+onRenderClient.js
+++ renderer/+onRenderClient.js
@@ -2,13 +2,13 @@ export { onRenderClient }
 
 import { hydrate } from '../output/Run'
 import { defineCustomElements } from 'wc-discord-message/loader/index.es2017.mjs'
+defineCustomElements(window)
 
 let dekuRoute
 
 async function onRenderClient(pageContext) {
   if (pageContext.isHydration) {
     // discards unsubscribe
-    defineCustomElements(window)
     const { routing } = hydrate(pageContext.dekuHydrationData)(
       pageContext.Page,
     )()
