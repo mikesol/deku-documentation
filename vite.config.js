d3ad28c33c37f58a9ce151e1afdfcab44cd13427
Mike Solomon <mike.solomon@hey.com>
2024-08-05 08:41:37 +0300

    Adds vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import path from 'path';
 import vike from 'vike/plugin';
 import deku from './rollup-plugin-vike-deku';
+import vercel from 'vite-plugin-vercel';
 
 export default {
-  plugins: [deku(), vike()],
+  plugins: [deku(), vike(), vercel()],
   resolve: {
     alias: {
       PureScript: path.resolve(__dirname, "output"),
