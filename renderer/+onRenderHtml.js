867274f3e9a223d11cb553aba9870da2cae8acd4
Mike Solomon <mike.solomon@hey.com>
2024-08-05 09:33:18 +0300

    Reactivates prerendering

--- renderer/+onRenderHtml.js
+++ renderer/+onRenderHtml.js
@@ -5,7 +5,7 @@ import { escapeInject, dangerouslySkipEscape } from "vike/server";
 import { ssr } from "../output/Run";
 
 async function onRenderHtml(pageContext) {
-  jsdomGlobal({ pretendToBeVisual: true });
+  jsdomGlobal(undefined, { pretendToBeVisual: true });
   document.getElementsByTagName("html")[0].innerHTML =
     "<head></head><body></body>";
 
