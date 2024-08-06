192f7a816dc090b132dc5a6c726fd41506e9211a
Mike Solomon <mike.solomon@hey.com>
2024-08-06 06:02:56 +0300

    Small text nit

--- src/Pages/CoreConcepts/Components/AddingEventHandlers/UsingTheOriginalEvent.purs
+++ src/Pages/CoreConcepts/Components/AddingEventHandlers/UsingTheOriginalEvent.purs
@@ -10,14 +10,14 @@ import Examples as Examples
 
 usingTheOriginalEvent :: Subsection
 usingTheOriginalEvent = subsection
-  { title: "Using the original event"
+  { title: "Using the DOM event"
   , matter: do
       example <- getExample StartCollapsed Nothing
         Examples.UsingTheOriginalEvent
       pure
         [ D.p_
             [ text_
-                "To use the original event, you can use the following syntax."
+                "To use the original event emitted by the DOM, you can use the following syntax."
             ]
         , example
         ]
