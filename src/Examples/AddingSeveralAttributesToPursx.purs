1b42c53254a64bcee491f71024a69031562c33cb
Mike Solomon <mike.solomon@hey.com>
2024-08-06 06:34:30 +0300

    Better fill

--- src/Examples/AddingSeveralAttributesToPursx.purs
+++ src/Examples/AddingSeveralAttributesToPursx.purs
@@ -26,8 +26,8 @@ type MyHtml =
       <div class="flex items-center">
         <span class="cursor-pointer text-gray-400 hover:text-gray-500">
           <!-- Heroicon name: mini/home -->
-          <svg class="h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clip-rule="evenodd" />
+          <svg class="h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" aria-hidden="true">
+            <path fill-rule="evenodd" fill="#7393B3" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clip-rule="evenodd" />
           </svg>
           <span class="sr-only cursor-pointer">Home</span>
         </span>
@@ -36,8 +36,8 @@ type MyHtml =
 
     <li ~projectsAtts~>
       <div class="flex items-center">
-        <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+        <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path fill="#7393B3" d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
         </svg>
         <span class="cursor-pointer ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Projects</span>
       </div>
@@ -45,8 +45,8 @@ type MyHtml =
 
     <li ~neroAtts~>
       <div class="flex items-center">
-        <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+        <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path fill="#7393B3" d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
         </svg>
         <span class="cursor-pointer ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Project Nero</span>
       </div>
